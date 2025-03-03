9782aa2d3df578615d652c55ae95a8d1b7d67d40
narciss14 <rubix684@gmail.com>
2025-03-03 23:21:07 +0500

    Add files via upload

---
+++ scenes/combat/hurt_box.gd
@@ -0,0 +1,44 @@
+extends Area2D
+
+const HitEffect = preload("res://effects/hit_effect.tscn")
+
+@onready var timer: Timer = $Timer
+
+signal invincibility_started
+signal invincibility_ended
+
+var invincible = false:
+	get:
+		return invincible
+	set(value):
+		invincible = value
+		if invincible:
+			emit_signal("invincibility_started")
+		else:
+			emit_signal("invincibility_ended")
+
+
+func create_hit_effect() -> void:
+	var hitEffect = HitEffect.instantiate()
+	hitEffect.global_position = global_position
+	get_tree().current_scene.add_child(hitEffect)
+
+
+func start_invincibility(duration):
+	if !self.invincible:
+		self.invincible = true
+		timer.start(duration)
+
+
+func _on_timer_timeout() -> void:
+	self.invincible = false
+
+
+func _on_invincibility_started() -> void:
+	set_deferred("monitoring", false)
+	set_deferred("monitorable", false)
+
+
+func _on_invincibility_ended() -> void:
+	set_deferred("monitoring", true)
+	set_deferred("monitorable", true)
---
+++ world/grass.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+const GrassEffect = preload("res://effects/grass_effect.tscn")
+
+
+func _on_hurt_box_area_entered(area: Area2D) -> void:
+	var grassEffect = GrassEffect.instantiate()
+	var main = get_tree().current_scene
+	main.add_child(grassEffect)
+	grassEffect.global_position = global_position
+	queue_free()
